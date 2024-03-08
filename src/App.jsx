9238d87d2eff94a4124a8a75e6bda444d648652d
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-08 13:43:56 +0100

    Tailwind css is working

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
     </h1>
   )
